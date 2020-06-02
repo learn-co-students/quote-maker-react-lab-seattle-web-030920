5455a677602cb1d50dc29d20a0b4f0838045b53b
dizhengcindy <dizhengcindy@gmail.com>
2020-06-02 13:55:34 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
 export default (state = [], action) => {
+  let idx
+  let newState
+
+  switch(action.type){
+    case "ADD_QUOTE":
+      return [...state,action.quote]
+      case "REMOVE_QUOTE":
+        return state.filter(quote => quote.id !== action.quoteId)
+      case "UPVOTE_QUOTE":
+         newState = [...state]
+         idx = state.findIndex(quote => quote.id === action.quoteId)
+        newState[idx].votes+=1
+        return newState
+      case "DOWNVOTE_QUOTE":
+        newState = [...state]
+        idx = state.findIndex(quote => quote.id === action.quoteId)
+        if(newState[idx].votes>0){
+          newState[idx].votes -=1}
+        return newState
+
+  default:
   return state;
+  }
 }
